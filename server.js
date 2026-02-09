0fd4b7285da8dc110d763db70323db52ddd7365c
Marcelo Ferreira De Araujo <marcelo@Marcelos-Mac-mini.local>
2026-02-09 14:02:19 -0300

    Add server.js proxy for Hostinger deployment

---
+++ server.js
@@ -0,0 +1,6 @@
+/**
+ * Hostinger Entry Point Proxy
+ * This file allows Hostinger to find a standard entry point (server.js)
+ * that points to our compiled TypeScript server.
+ */
+import "./dist/index.cjs";
